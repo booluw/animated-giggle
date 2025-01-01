f263fbbade2500ef23696326c757347c2923c22e
= <=>
2025-01-01 12:20:52 +0100

    feat: add charting capabilities and random color generation
    - Integrated Chart.js and vue-chartjs for data visualization in HomeView.
    - Added a Doughnut chart to display transaction types.
    - Implemented getRandomColor function for dynamic color generation in the UI.
    - Updated package.json and yarn.lock to include chart.js and vue-chartjs dependencies.

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,11 @@
 <script lang="ts" setup>
 import { computed, onMounted, ref } from 'vue'
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js'
+import { Doughnut } from 'vue-chartjs'
 import { useAppStore } from '@/stores/app'
 
 import { fetchSummary } from '@/services/dashboard'
-import { formatMoney, formatNumber } from '@/utils/functions'
+import { formatMoney, formatNumber, getRandomColor } from '@/utils/functions'
 
 import Btn from '@/components/Btn.vue'
 import IconsAdd from '@/components/icons/IconsAdd.vue'
@@ -14,6 +16,8 @@ import IconsGivingHand from '@/components/icons/IconsGivingHand.vue'
 import IconsPiggybank from '@/components/icons/IconsPiggybank.vue'
 import IconsTimer from '@/components/icons/IconsTimer.vue'
 import Loader from '@/components/Loader.vue'
+
+ChartJS.register(ArcElement, Tooltip, Legend)
 
 const store = useAppStore()
 const loading = ref(false)
@@ -32,15 +36,15 @@ const getSummary = async function () {
   loading.value = false
 }
 
-onMounted(() => getSummary())
+onMounted(async () => {
+  await getSummary()
+})
 </script>
 <template>
   <div v-if="loading" class="h-screen flex items-center justify-center">
     <div class="flex gap-3 flex-col items-center justify-center">
       <Loader />
-      <p class="opacity-75 animate-pulse">
-        Loading dashboard, please wait...
-      </p>
+      <p class="opacity-75 animate-pulse">Loading dashboard, please wait...</p>
     </div>
   </div>
   <main v-else class="w-full flex flex-col gap-[20px]">
@@ -48,6 +52,7 @@ onMounted(() => getSummary())
       <h1 class="text-[32px] font-bold">Dashboard</h1>
       <Btn size="default" :icon="IconsAdd"> New Transaction </Btn>
     </div>
+
     <div class="flex gap-[30px]">
       <div
         class="rounded-[18px] bg-gradient-to-b from-[#140800] to-[#7A3100] p-[20px] w-[280px] flex flex-col gap-[19px]"
@@ -150,6 +155,38 @@ onMounted(() => getSummary())
               <h3 class="text-xs font-normal text-[#665E59]">Avg. Execution time</h3>
               {{ Number(summary?.avg_execution_time).toFixed(0) }} days
             </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="flex gap-[30px] pb-5">
+      <div class="w-[354px] rounded-[8px] border py-[20px] px-[24px]">
+        <h3 class="font-[500]">Transaction by product</h3>
+        <Doughnut
+          :options="{ responsive: true, maintainAspectRatio: true, plugins: { legend: { position: 'bottom' } } }"
+          :data="{
+            labels: summary?.transaction_type_summary.map((soc) => soc.transaction_type),
+            datasets: [
+              {
+                backgroundColor: ['#CC5500', '#007ACC', '#4D3626'],
+                data: summary?.transaction_type_summary.map((soc) => Number(soc.count)),
+              },
+            ],
+          }"
+        />
+      </div>
+      <div class="w-full">
+        <h3 class="font-[500] mb-1">Transaction by awarding companies</h3>
+        <div class="border rounded p-2 flex flex-wrap gap-4">
+          <div
+            class="w-auto text-white px-[28px] py-[20px] min-h-44"
+            :class="{ '!w-[400px]': key === 0 }"
+            :style="`background-color: ${getRandomColor()}!important;`"
+            v-for="(company, key) in summary?.awarding_company_summary"
+            :key
+          >
+            {{ company.awarding_company }}
           </div>
         </div>
       </div>
